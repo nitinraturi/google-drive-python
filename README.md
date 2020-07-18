8b061fc24ff47bd1787e3e3cec9a52e6f9915fe2
nitin <raturi002@gmail.com>
2020-07-18 12:16:52 +0530

    Issue fixed: Pagination of files

---
+++ README.md
@@ -0,0 +1,8 @@
+# google-drive-python
+
+## Installation
+- pip install -r requirements.txt
+- mkdir downloads
+
+## Features
+- Provide a google drive public folder url to the script and it will fetch all the result inside that folder in a downloads directory at the same path.
--- main.py
+++ main.py
@@ -1,171 +1,219 @@
 import pickle
-import os, io
+import os
+import io
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from tabulate import tabulate
 from googleapiclient.http import MediaIoBaseDownload
-from decouple import config
+import sys
 from hurry.filesize import size, verbose
 from apiclient import errors
 
 
-# Scope of getting the full access from google drive of a user
-SCOPES = ['https://www.googleapis.com/auth/drive']
+class Drive():
+    """Drive url"""
+    # Functions:
+    # get_gdrive_service()
+    # downloadFile(file_id, path=None)
+    # getFileObject(filename)
+    # getFileObjectForFolder(filename, path)
+    # list_files(query_string, path=None):
+    # get_size_format(file_size):
+    # parseID(url):
+    # getMetaData( fileId):
+    # isFolder(url):
+    # download():
 
-# Function for getting the service object of google drive
-def get_gdrive_service():
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+    def __init__(self, url,
+                 initial_path=None, verbose=False):
+        # Scope of getting the full access from google drive of a user
+        self.SCOPES = ['https://www.googleapis.com/auth/drive']
+        self.service = self.get_gdrive_service()
+        self.URL = url
+        self.verbose = verbose
+        if initial_path is None:
+            self.initial_path = os.path.join(os.getcwd(), "downloads")
+            if not os.path.exists(self.initial_path):
+                os.mkdir(self.initial_path)
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    # return Google Drive API service
-    return build('drive', 'v3', credentials=creds)
+            self.initial_path = initial_path
 
-# Function for download the google drive files
-def downloadFile(file_id, path=None):
-    global service;
-    fileName = getMetaData(file_id)['name']
-    if path is None:
-        # get the file object in write binary mode
-        fileObj = getFileObject(fileName)
-    else:
-        # get the file object in write binary mode
-        fileObj = getFileObjectForFolder(fileName, path)
-    # call the service api for downloading the media
-    request = service.files().get_media(fileId=file_id)
-    # request the media downloader
-    downloader = MediaIoBaseDownload(fileObj, request)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-        # Track the downloading progress
-        print("[*]Download Progress {0}% - {1}.".format(str(int(status.progress())*100),fileName))
-
-# Function for getting the file object opened
-def getFileObject(filename):
-    return open(os.path.join(os.getcwd(),"downloads", filename), 'wb')
-
-# Function for getting the file object opened
-def getFileObjectForFolder(filename, path):
-    print(path+filename)
-    return open(path+'/'+filename, 'wb')
-
-# Function for the listing the google drive files
-def list_files(query_string, path=None):
-    """given items returned by Google Drive API, prints them in a tabular way"""
-    global service;
-    items = service.files().list(
-        q=query_string,
-        pageSize=10, fields="nextPageToken, files(id, name, mimeType, size, parents, modifiedTime)").execute()
-    # get the results
-    items = items.get('files', [])
-    if not items:
-        # empty drive
-        print('No files found.')
-    else:
-        rows = []
-        for item in items:
-            if item['mimeType'] == 'application/vnd.google-apps.folder':
-                if not os.path.isdir(os.path.join(os.getcwd(), item['name'])):
-                    os.mkdir(os.path.join(os.getcwd(), item['name']))
-                list_files(query_string="'{0}' in parents".format(item['id']), path=os.path.join(os.getcwd(), item['name']))
+    def get_gdrive_service(self):
+        """Gets the service object of google drive"""
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
             else:
-                # get the File ID
-                id = item["id"]
-                if path is None:
-                    downloadFile(id)
-                else:
-                    downloadFile(id, path)
-                # get the name of file
-                name = item["name"]
-                try:
-                    # parent directory ID
-                    parents = item["parents"]
-                except:
-                    # has no parrents
-                    parents = "N/A"
-                try:
-                    # get the size in nice bytes format (KB, MB, etc.)
-                    size = get_size_format(int(item["size"]))
-                except:
-                    # not a file, may be a folder
-                    size = "N/A"
-                # get the Google Drive type of file
-                mime_type = item["mimeType"]
-                # get last modified date time
-                modified_time = item["modifiedTime"]
-                # append everything to the list
-                rows.append((id, name, parents, size, mime_type, modified_time))
-        print("Files:")
-        # convert to a human readable table
-        table = tabulate(
-            rows, headers=["ID", "Name", "Parents", "Size", "Type", "Modified Time"])
-        # print the table
-        print("table",table)
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        # return Google Drive API service
+        return build('drive', 'v3', credentials=creds)
 
-# Function for formatting the file sizes
-def get_size_format(file_size):
-    return size(file_size, system=verbose)
+    def downloadFile(self, file_id, initial_path=None):
+        """Function to download the google drive files"""
 
-# Function for retrieving the ID from given url
-def parseID(url):
-    return url.split('/')[-1].split('?')[0]
-
-# Function for retrieving the files attributes
-def getMetaData(fileId):
-    global service;
-    try:
-        return service.files().get(fileId=fileId).execute()
-    except errors.HttpError as e:
-        print("An error occurered {}".format(e))
-
-# Function for check the url is of folder
-def checkFolderURL(url):
-    global service;
-    folderId = parseID(url)
-    try:
-        file = getMetaData(folderId)
-        if file['mimeType'] == 'application/vnd.google-apps.folder':
-            return True
+        fileName = self.getMetaData(file_id)['name']
+        # if self.verbose:
+        if initial_path is None:
+            long_name = os.path.join(
+                self.initial_path, initial_path or '', fileName)
+            if os.path.exists(long_name):
+                return
+            # get the file object in write binary mode
+            fileObj = self.getFileObject(fileName)
         else:
-            return False
-    except Exception as e:
-        pass
+            # get the folder object in write binary mode
+            print("recieved folder", initial_path, fileName)
+            fileObj = self.getFileObjectForFolder(fileName, initial_path)
+        # call the service api for downloading the media
+        request = self.service.files().get_media(fileId=file_id)
+        # request the media downloader
+        downloader = MediaIoBaseDownload(fileObj, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            # Track the downloading progress
+            print(
+                "[*]Download Progress {0}% - {1}.".format(str(int(status.progress())*100), fileName))
 
-# Main entry point
-def main(url):
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 5 files the user has access to.
-    """
-    global service;
-    service = get_gdrive_service()
-    if checkFolderURL(url):
-        # Call the Drive v3 API
-        folderId = parseID(url)
-        q = "'{0}' in parents".format(folderId)
-        # list all 20 files & folders
-        list_files(q)
-    else:
-        print("Please give the url of a folder")
+    def getFileObject(self, filename):
+        """Open the destination file in write mode"""
+        return open(os.path.join(self.initial_path, filename), 'wb')
+
+    def getFileObjectForFolder(self, filename, path):
+        """Open the destination folder with an existing initial path in write mode"""
+        return open(os.path.join(self.initial_path, path, filename), 'wb')
+
+    def list_files(self, query_string, path=None, nextPageToken=None):
+        """List the google drive files as returned by Google Drive API. 
+        If verbose: then prints in tabular format."""
+
+        if nextPageToken:
+            items = self.service.files().list(
+                q=query_string,
+                pageToken=nextPageToken,
+                pageSize=1000, fields="nextPageToken, files(id, name, mimeType, size, parents, modifiedTime)").execute()
+        else:
+            items = self.service.files().list(
+                q=query_string,
+                pageSize=1000, fields="nextPageToken, files(id, name, mimeType, size, parents, modifiedTime)").execute()
+        # get the results
+
+        nextPageToken=items.get('nextPageToken')
+        items = items.get('files', [])
+        if not items:
+            # empty drive
+            print(f'No files found in query_string "{query_string}".')
+        else:
+            rows = []
+            for item in items:
+                if item['mimeType'] == 'application/vnd.google-apps.folder':
+                    folder = os.path.join(os.getcwd(), item['name'])
+                    if not os.path.isdir(folder):
+                        os.mkdir(folder)
+                    self.list_files(query_string="'{0}' in parents".format(
+                        item['id']), path=folder)
+                else:
+                    # get the File ID
+                    id = item["id"]
+                    if path is None:
+                        self.downloadFile(id)
+                    else:
+                        self.downloadFile(id, path)
+                    if self.verbose:
+                        # get the name of file
+                        name = item["name"]
+                        try:
+                            # parent directory ID
+                            parents = item["parents"]
+                        except:
+                            # has no parrents
+                            parents = "N/A"
+                        try:
+                            # get the size in nice bytes format (KB, MB, etc.)
+                            size = get_size_format(int(item["size"]))
+                        except:
+                            # not a file, may be a folder
+                            size = "N/A"
+                        # get the Google Drive type of file
+                        mime_type = item["mimeType"]
+                        # get last modified date time
+                        modified_time = item["modifiedTime"]
+                        # append everything to the list
+                        rows.append((id, name, parents, size,
+                                     mime_type, modified_time))
+
+            if self.verbose:
+                print("Files:")
+                # convert to a human readable table
+                table = tabulate(
+                    rows, headers=["ID", "Name", "Parents", "Size", "Type", "Modified Time"])
+                # print the table
+                print("table", table)
+            
+            if nextPageToken:
+                print(f'Fetching next set of results".')
+                folderId = self.parseID(self.URL)
+                q = "'{0}' in parents".format(folderId)
+                self.list_files(q,nextPageToken=nextPageToken)
+
+    def get_size_format(self, file_size):
+        """Returns the file sizes of given file"""
+        return size(file_size, system=verbose)
+
+    def parseID(self, url):
+        """Retrieve the ID from given url"""
+        return url.split('/')[-1].split('?')[0]
+
+    def getMetaData(self, fileId):
+        """Retrieve the files attributes"""
+        try:
+            return self.service.files().get(fileId=fileId).execute()
+        except errors.HttpError as exception:
+            raise errors.HttpError(f"An error occurered {exception}")
+
+    def isFolder(self, url):
+        """Function to check if the url is a folder"""
+        folderId = self.parseID(url)
+        try:
+            file = self.getMetaData(folderId)
+            if file['mimeType'] == 'application/vnd.google-apps.folder':
+                return True
+            else:
+                return False
+        except Exception as e:
+            pass
+
+    def download(self):
+        """Main entry point """
+        if self.isFolder(self.URL):
+            # Call the Drive v3 API
+            folderId = self.parseID(self.URL)
+            q = "'{0}' in parents".format(folderId)
+            # list all files & folders
+            self.list_files(q)
+        else:
+            raise ValueError("Please give the url of a folder")
+
+
+def downloadFromGDrive(url, path=None):
+    drive = Drive(url=str(url), initial_path=path)
+    drive.download()
 
 
 if __name__ == "__main__":
-    global service;
-    # Folder url from environment file
-    url = config('FOLDER_URL', cast=str)
-    main(url)
+    FOLDER_URL="https://drive.google.com/drive/folders/1-cmUKkdiDwRz2otQfmGJf-NhDMAWqM1S?usp=sharing"
+    downloadFromGDrive(FOLDER_URL)
